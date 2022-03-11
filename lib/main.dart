2469f68274415b2415a76f161a70899a61bef5ad
bumis95 <bumis95@gmail.com>
2022-03-11 15:56:46 +0300

    Add firebase auth

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,11 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
-import 'package:my_books/presentation/login_screen.dart';
+import 'package:my_books/presentation/ui/login_screen.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(
     MaterialApp(
       onGenerateTitle: (BuildContext context) =>
