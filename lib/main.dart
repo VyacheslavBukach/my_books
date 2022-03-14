230904df30b76e5eae0e9394ec250ceabef207a6
bumis95 <bumis95@gmail.com>
2022-03-14 15:49:08 +0300

    Keep stay signUp

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,11 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:my_books/blocs/auth_bloc/auth_bloc.dart';
 import 'package:my_books/data/repositories/firebase_auth_repository_impl.dart';
-import 'package:my_books/domain/repositories/auth_repository.dart';
+import 'package:my_books/presentation/ui/books_screen.dart';
 import 'package:my_books/presentation/ui/sign_in_screen.dart';
 
 Future<void> main() async {
@@ -30,7 +31,16 @@ class MyApp extends StatelessWidget {
               AppLocalizations.of(context)?.app_name ?? '',
           localizationsDelegates: AppLocalizations.localizationsDelegates,
           supportedLocales: AppLocalizations.supportedLocales,
-          home: const SignInScreen(),
+          home: StreamBuilder<User?>(
+            stream: FirebaseAuth.instance.authStateChanges(),
+            builder: (context, snapshot) {
+              if (snapshot.hasData) {
+                return const BooksScreen();
+              }
+
+              return const SignInScreen();
+            },
+          ),
         ),
       ),
     );
