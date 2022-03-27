7b78826bd524804a5118523c50784525472798f9
bumis95 <bumis95@gmail.com>
2022-03-27 17:12:20 +0300

    Style book element

--- lib/presentation/ui_components/book_card.dart
+++ lib/presentation/ui_components/book_card.dart
@@ -13,6 +13,9 @@ class BookCard extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Material(
+      elevation: 8,
+      borderRadius: BorderRadius.circular(8),
+      clipBehavior: Clip.antiAliasWithSaveLayer,
       child: InkWell(
         splashColor: Colors.white.withOpacity(0.5),
         child: Ink.image(
