a9c61984a4939b1ed6ac76f5e051216f25c8614b
bumis95 <bumis95@gmail.com>
2022-05-02 16:32:59 +0300

    Add glance effect

--- lib/presentation/ui_components/search_result_item.dart
+++ lib/presentation/ui_components/search_result_item.dart
@@ -1,3 +1,5 @@
+import 'dart:ui';
+
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
@@ -15,31 +17,54 @@ class SearchResultItem extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return ListTile(
-      tileColor: Colors.white,
-      leading: CachedNetworkImage(
-        imageUrl: book.posterUrl,
-        imageBuilder: (context, imageProvider) => Image(
-          image: imageProvider,
+    return ClipRRect(
+      child: BackdropFilter(
+        filter: ImageFilter.blur(
+          sigmaX: 15,
+          sigmaY: 15,
         ),
-        placeholder: (context, url) => const Center(
-          child: CircularProgressIndicator(),
-        ),
-      ),
-      title: Text(
-        book.title,
-        style: GoogleFonts.robotoSlab(
-          fontWeight: FontWeight.bold,
-        ),
-      ),
-      subtitle: Text(
-        book.author,
-        style: GoogleFonts.robotoSlab(),
-      ),
-      onTap: () => Navigator.of(context).push(
-        MaterialPageRoute(
-          builder: (context) => BookDetailScreen(
-            bookID: book.id,
+        child: Container(
+          decoration: BoxDecoration(
+            gradient: const LinearGradient(
+              begin: Alignment.topLeft,
+              end: Alignment.bottomCenter,
+              colors: [
+                Colors.white60,
+                Colors.white10,
+              ],
+            ),
+            borderRadius: const BorderRadius.all(
+              Radius.circular(20),
+            ),
+            border: Border.all(width: 2, color: Colors.white30),
+          ),
+          child: ListTile(
+            leading: CachedNetworkImage(
+              imageUrl: book.posterUrl,
+              imageBuilder: (context, imageProvider) => Image(
+                image: imageProvider,
+              ),
+              placeholder: (context, url) => const Center(
+                child: CircularProgressIndicator(),
+              ),
+            ),
+            title: Text(
+              book.title,
+              style: GoogleFonts.robotoSlab(
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            subtitle: Text(
+              book.author,
+              style: GoogleFonts.robotoSlab(),
+            ),
+            onTap: () => Navigator.of(context).push(
+              MaterialPageRoute(
+                builder: (context) => BookDetailScreen(
+                  bookID: book.id,
+                ),
+              ),
+            ),
           ),
         ),
       ),
