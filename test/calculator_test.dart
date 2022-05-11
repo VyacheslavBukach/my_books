14fcd1e7bd51763575d5ee9760cc8a92103aaeea
bumis95 <bumis95@gmail.com>
2022-05-11 15:38:36 +0300

    Fix calculator test

--- test/calculator_test.dart
+++ test/calculator_test.dart
@@ -2,27 +2,45 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:my_books/calculator.dart';
 
 void main() {
-  test('Sum_5Plus2_Returned7', () {
-    final calculator = Calculator();
-    var result = calculator.sum(number1: 5, number2: 2);
-    expect(result, 7);
+  late Calculator calculator;
+
+  setUp(() {
+    calculator = Calculator();
   });
 
-  test('Difference_5Minus2_Returned3', () {
-    final calculator = Calculator();
-    var result = calculator.difference(number1: 5, number2: 2);
-    expect(result, 3);
+  test('Sum_Test', () {
+    var result1 = calculator.sum(number1: 1, number2: 1);
+    expect(result1, 2);
+    var result2 = calculator.sum(number1: 2, number2: 2);
+    expect(result2, 4);
+    var result3 = calculator.sum(number1: 3, number2: 3);
+    expect(result3, 6);
   });
 
-  test('Multiply_5Multiply2_Returned10', () {
-    final calculator = Calculator();
-    var result = calculator.multiply(number1: 5, number2: 2);
-    expect(result, 10);
+  test('Difference_Test', () {
+    var result1 = calculator.difference(number1: 1, number2: 1);
+    expect(result1, 0);
+    var result2 = calculator.difference(number1: 2, number2: 2);
+    expect(result2, 0);
+    var result3 = calculator.difference(number1: 3, number2: 3);
+    expect(result3, 0);
   });
 
-  test('Divide_5DivideBy2_Returned2.5', () {
-    final calculator = Calculator();
-    var result = calculator.divide(number1: 5, number2: 2);
-    expect(result, 2.5);
+  test('Multiply_Test', () {
+    var result1 = calculator.multiply(number1: 1, number2: 1);
+    expect(result1, 1);
+    var result2 = calculator.multiply(number1: 2, number2: 2);
+    expect(result2, 4);
+    var result3 = calculator.multiply(number1: 3, number2: 3);
+    expect(result3, 9);
+  });
+
+  test('Divide_Test', () {
+    var result1 = calculator.divide(number1: 1, number2: 1);
+    expect(result1, 1);
+    var result2 = calculator.divide(number1: 2, number2: 2);
+    expect(result2, 1);
+    var result3 = calculator.divide(number1: 3, number2: 3);
+    expect(result3, 1);
   });
 }
